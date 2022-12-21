138b66fe19ee307797db2658a3b99b01abb240f5
Jason <jason.juan@theconsultancyllc.com>
2022-12-21 13:22:07 -0800

    Commit

---
+++ .sfdx/typings/lwc/apex/ContactSearchCntlr.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/ContactSearchCntlr.fetchContact" {
+  export default function fetchContact(param: {searchParamsJSON: any}): Promise<any>;
+}
---
+++ .sfdx/typings/lwc/apex/DisplayController.d.ts
@@ -0,0 +1,15 @@
+declare module "@salesforce/apex/DisplayController.getProgramSession" {
+  export default function getProgramSession(param: {id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DisplayController.getPageLayout" {
+  export default function getPageLayout(param: {pageLayoutList: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DisplayController.getProgramNarrative" {
+  export default function getProgramNarrative(param: {id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DisplayController.getUserInfo" {
+  export default function getUserInfo(): Promise<any>;
+}
+declare module "@salesforce/apex/DisplayController.finishSession" {
+  export default function finishSession(param: {id: any}): Promise<any>;
+}
