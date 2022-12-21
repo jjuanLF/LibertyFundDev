edc8652295702827514d8709a399502fed38e0bf
Jason <jason.juan@theconsultancyllc.com>
2022-12-21 13:14:24 -0800

    committed

--- .sfdx/typings/lwc/apex/ContactSearchCntlr.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/ContactSearchCntlr.fetchContact" {
-  export default function fetchContact(param: {searchParamsJSON: any}): Promise<any>;
-}
--- .sfdx/typings/lwc/apex/CustomPathController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/CustomPathController.getDependentPicklistValues" {
-  export default function getDependentPicklistValues(param: {sObjectName: any, fieldName: any}): Promise<any>;
-}
-declare module "@salesforce/apex/CustomPathController.updateSubStage" {
-  export default function updateSubStage(param: {id: any, value: any}): Promise<any>;
-}
